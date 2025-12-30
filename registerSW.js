039938f49300b55f4bd900e3177a271bdee66f6f
anas99910 <44525982+anas99910@users.noreply.github.com>
2025-12-30 02:20:39 +0100

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/bitkhzin/sw.js', { scope: '/bitkhzin/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
